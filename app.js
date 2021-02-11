90a1ec6171590e043f03d853ffbb6202c9cb4c59
Steve Min <stevekmin@gmail.com>
2021-02-11 08:27:07 -0800

    initialize playing variables

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello World');
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const scoreDisplay = document.querySelector('#score');
+  const startBtn = document.querySelector('#start-button');
+  const width = 10;
 });
