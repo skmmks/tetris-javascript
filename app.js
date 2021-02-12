0a16adee094bf2a7ae4e23034da22d1fdbb00e7e
Steve Min <stevekmin@gmail.com>
2021-02-12 08:12:55 -0800

    create 'i' tetris piece

--- app.js
+++ app.js
@@ -33,3 +33,10 @@ const oTetromino = [
   [0, 1, width, width + 1],
   [0, 1, width, width + 1],
 ];
+
+const iTetromino = [
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+];
