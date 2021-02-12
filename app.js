525f7861cdf189d386a233a6c2f04a681b682557
Steve Min <stevekmin@gmail.com>
2021-02-12 08:11:03 -0800

    create 'z' tetris piece

--- app.js
+++ app.js
@@ -12,3 +12,10 @@ const lTetromino = [
   [1, width + 1, width * 2 + 1, width * 2],
   [width, width * 2, width * 2 + 1, width * 2 + 2],
 ];
+
+const zTetromino = [
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+];
