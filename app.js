47ca66e96e69f4ce7ca105f8931944e96760f812
Steve Min <stevekmin@gmail.com>
2021-03-05 08:33:17 -0800

    change game over message

--- app.js
+++ app.js
@@ -193,7 +193,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const gameOver = () => {
     if (current.some((index) => squares[currentPosition + index].classList.contains('taken'))) {
-      scoreDisplay.innerHTML = 'end';
+      scoreDisplay.innerHTML = 'Game Over';
       clearInterval(timerId);
     }
   };
